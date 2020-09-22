95ad3ce00baae4663830e2a7fd37a62de97e455b
abbas jafari <abbas.jafari@powercoders.org>
2020-09-22 16:01:00 +0200

    added files

--- week3/Tuesday/Exercise1/sort.py
+++ week3/Tuesday/Exercise1/sort.py
@@ -7,11 +7,50 @@ Input = []
 # asking for  input
 
 
-while True:
-    N = input("how many numbers you want to sort:")
-    if isinstance(N, int):
-        for i in N:
-            Input.append(input(f'enter {i}th number:'))
+def main():
+    while True:
+        N = input("how many numbers you want to sort:")
+        print(type(N))
+        if isinstance(int(N), int):
+            for i in range(int(N)):
+                while True:
+                    try:
+                        Input.append(int(input(f'enter {i + 1}th number:')))
+                        break
+                    except ValueError:
+                        print("please try again with a number")
+
+        else:
+            print("input is invalid please try again with a number or exit")
+        print(Input)
+        time.sleep(1)
+        quick_sort(Input)
+        for number in Input:
+            print(Input.pop())
+
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
     else:
-        print("input is invalid please try again or exit")
-    time.sleep(2)
+        left = []
+        right = []
+        new_array = []
+        pivot = array.pop()
+        length = len(array)
+
+        for i in range(length):
+            d1 = array[i]
+            d2 = pivot
+            if d1 <= d2:
+                left.append(array[i])
+            else:
+                right.append(array[i])
+        left = quick_sort(left)
+        left.append(pivot)
+        right = quick_sort(right)
+        print(left)
+        print(right)
+        return left.append(right)
+
+main()
---
+++ week3/Tuesday/hour_of_code/README.md
@@ -0,0 +1,7 @@
+Update the HTML and CSS files so that 
+1. the HTML is semantic - what about the divs (5 min)
+2. no classes are needed for the css (10 min)
+3. the page looks like the final pdfs mobile and desktop (45 min)
+
+Estimated time: 60 minutes
+Total points: 60
